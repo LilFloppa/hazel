7feff6c8fbb69ab34b95e1615696c71476c1c008
Nikita Skrynnik <nikita.nstu@gmail.com>
2020-06-24 19:05:05 +0700

    Add logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,13 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	HZ_INFO("Hello!");
+
+	int a = 5;
+	HZ_ERROR("A = {0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
