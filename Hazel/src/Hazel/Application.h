83c77e43e369fcd2e528854dc38808b612753a78
Nikita Skrynnik <nikita.nstu@gmail.com>
2020-06-24 23:13:55 +0700

    Add Window, Events

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,6 +1,6 @@
 #pragma once
-
-#include "Core.h"
+#include "Hazel/Core.h"
+#include "Hazel/Window.h"
 
 namespace Hazel
 {
@@ -11,6 +11,9 @@ namespace Hazel
 		virtual ~Application();
 
 		void Run();
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 
--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <memory>
-
-#include "Core.h"
+#include "Hazel/Core.h"
 #include "spdlog/spdlog.h"
+#include "spdlog/fmt/ostr.h"
 
 namespace Hazel
 {
