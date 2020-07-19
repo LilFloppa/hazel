8e657dff192a513ea6c3ffb54222b19ca098dedb
Nikita.Skrynnik <nikita.skrynnik@dasha.ai>
2020-07-19 22:19:44 +1100

    Add blending

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,6 +1,8 @@
 #include "hzpch.h"
 #include "Application.h"
 
+#include "Hazel/Renderer/Renderer.h"
+
 #include <GLFW/glfw3.h>
 
 namespace Hazel
@@ -15,6 +17,9 @@ namespace Hazel
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(HZ_BIND_EVENT_FN(Application::OnEvent));
 		m_Window->SetVSync(false);
+
+
+		Renderer::Init();
 
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -151,7 +151,8 @@ public:
 		m_Camera = Hazel::OrthographicCamera(-1.6f, 1.6f, -0.9f, 0.9f);
 
 		
-		m_Texture = Hazel::Texture2D::Create("assets/textures/checkerboard.jpg");
+		m_Texture = Hazel::Texture2D::Create("assets/textures/Checkerboard.png");
+		m_ChernoLogoTexture = Hazel::Texture2D::Create("assets/textures/ChernoLogo.png");
 
 		std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -224,6 +225,9 @@ public:
 		}
 
 		m_Texture->Bind();
+		Hazel::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));		
+		
+		m_ChernoLogoTexture->Bind();
 		Hazel::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
 		Hazel::Renderer::EndScene();
@@ -256,6 +260,7 @@ private:
 	Hazel::Ref<Hazel::IndexBuffer> m_IndexBuffer;
 
 	Hazel::Ref<Hazel::Texture2D> m_Texture;
+	Hazel::Ref<Hazel::Texture2D> m_ChernoLogoTexture;
 
 	Hazel::Ref<Hazel::VertexArray> m_SquareVA;
 
