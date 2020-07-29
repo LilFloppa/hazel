791b4f3a09eeb082df17bf444a6a1e3ce7faf6ca
Nikita.Skrynnik <nikita.skrynnik@dasha.ai>
2020-07-29 23:41:07 +1100

    pull requests

--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,18 @@ namespace Hazel
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		HZ_CORE_ASSERT(status, "Failed to initialeze Glad!");
+
+		HZ_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		HZ_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+		#ifdef HZ_ENABLE_ASSERTS
+			int versionMajor;
+			int versionMinor;
+			glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+			glGetIntegerv(GL_MAJOR_VERSION, &versionMinor);
+
+			HZ_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Hazel requires at least OpenGL version 4.5");
+		#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
